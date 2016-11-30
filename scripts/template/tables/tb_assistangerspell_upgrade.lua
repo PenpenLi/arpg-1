32d8e09eba8714b19587bacf5a6766a742e1e80c
arpg <arpg@Center.(none)>
2016-11-30 17:55:21 +0800

    public version

--- scripts/template/tables/tb_assistangerspell_upgrade.lua
+++ scripts/template/tables/tb_assistangerspell_upgrade.lua
@@ -5,10 +5,10 @@
 
 tb_assistangerspell_upgrade = {
 	--  id:int	技能id
-	--  cost:int	金币消耗
+	--  cost:array	金币消耗
 	--  nextid:int	进阶的技能id
-	[13] = {id = 13,cost = 2000,nextid = 15,},
-	[14] = {id = 14,cost = 2000,nextid = 16,},
-	[15] = {id = 15,cost = 3000,nextid = 17,},
-	[16] = {id = 16,cost = 3000,nextid = 18,},
+	[13] = {id = 13,cost = {{2,2000}},nextid = 15,},
+	[14] = {id = 14,cost = {{2,2000}},nextid = 16,},
+	[15] = {id = 15,cost = {{2,3000}},nextid = 17,},
+	[16] = {id = 16,cost = {{2,3000}},nextid = 18,},
 }
--- scripts/template/tables/tb_learn_spell.lua
+++ scripts/template/tables/tb_learn_spell.lua
@@ -7,17 +7,18 @@ tb_learn_spell = {
 	--  id:int	技能ID
 	--  playerLevel:int	角色等级
 	--  item:array	所需道具
-	[11] = {id = 11,playerLevel = 2,item = {},},
-	[12] = {id = 12,playerLevel = 2,item = {},},
-	[13] = {id = 13,playerLevel = 2,item = {},},
-	[14] = {id = 14,playerLevel = 2,item = {},},
-	[15] = {id = 15,playerLevel = 0,item = {{50001,10}，{0,10}},},
-	[16] = {id = 16,playerLevel = 0,item = {{50001,10}，{0,10}},},
-	[17] = {id = 17,playerLevel = 0,item = {},},
-	[18] = {id = 18,playerLevel = 0,item = {},},
-	[19] = {id = 19,playerLevel = 3,item = {},},
-	[20] = {id = 20,playerLevel = 3,item = {},},
-	[21] = {id = 21,playerLevel = 4,item = {},},
-	[22] = {id = 22,playerLevel = 4,item = {},},
-	[1001] = {id = 1001,playerLevel = 4,item = {},},
+	--  resource:array	所需资源
+	[11] = {id = 11,playerLevel = 2,item = {},resource = {},},
+	[12] = {id = 12,playerLevel = 2,item = {},resource = {},},
+	[13] = {id = 13,playerLevel = 2,item = {},resource = {},},
+	[14] = {id = 14,playerLevel = 2,item = {},resource = {},},
+	[15] = {id = 15,playerLevel = 0,item = {{50001,10}},resource = {{0,10}},},
+	[16] = {id = 16,playerLevel = 0,item = {{50001,10}},resource = {{0,10}},},
+	[17] = {id = 17,playerLevel = 0,item = {},resource = {},},
+	[18] = {id = 18,playerLevel = 0,item = {},resource = {},},
+	[19] = {id = 19,playerLevel = 3,item = {},resource = {},},
+	[20] = {id = 20,playerLevel = 3,item = {},resource = {},},
+	[21] = {id = 21,playerLevel = 4,item = {},resource = {},},
+	[22] = {id = 22,playerLevel = 4,item = {},resource = {},},
+	[1001] = {id = 1001,playerLevel = 4,item = {},resource = {},},
 }
