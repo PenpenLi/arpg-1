a2bf8708d7076b439a06668ab1f1938142792e75
arpg <arpg@Center.(none)>
2016-12-09 20:10:42 +0800

    public version

--- scripts/template/tables/tb_divine_base.lua
+++ scripts/template/tables/tb_divine_base.lua
@@ -11,7 +11,6 @@ tb_divine_base = {
 	--  avtivedata:array	激活条件数据
 	--  time:int	限时
 	--  strength:int	可强化
-	[1] = {id = 1,skill = 1011,passiveskill = {{1012,3}},avtivetype = 3,avtivedata = {},time = 5,strength = 0,},
-	[2] = {id = 2,skill = 1011,passiveskill = {{1012,3},{11004,5}},avtivetype = 1,avtivedata = {{101,10}},time = -1,strength = 1,},
-	[3] = {id = 3,skill = 1013,passiveskill = {{1014,3}},avtivetype = 2,avtivedata = {{1,700}},time = -1,strength = 1,},
+	[1] = {id = 1,skill = 1011,passiveskill = {{1012,3}},avtivetype = 1,avtivedata = {{101,10}},time = -1,strength = 1,},
+	[2] = {id = 2,skill = 1013,passiveskill = {{1014,3}},avtivetype = 2,avtivedata = {{1,700}},time = -1,strength = 1,},
 }
